bb121e544f3bbc1ae5beaddb08192a2d3967c93e
Nahuel Carrizo <carrizonahue86@gmail.com>
2024-06-24 17:05:01 -0300

    Esta rama tiene un agregado de JavaScript

---
+++ Protecto final HTML y CSS/script.js
@@ -0,0 +1,18 @@
+const menu=document.querySelector('#menu-icon')
+const navList=document.querySelector('.navlist')
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x')
+    navList.classList.toggle('open')
+}
+
+const sr=ScrollReveal({
+    distance: '65px',
+    durarion:2000,
+    delay: 450,
+    reset: true
+})
+sr.reveal('.hero-text', {delay:200, origin:'top'})
+sr.reveal('.hero-img', {delay:450, origin:'top'})
+sr.reveal('.icons', {delay:500, origin:'left'})
+sr.reveal('scroll-down', {delay:450, origin:'right'})
